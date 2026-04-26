2f875194b0934ea2be667dbaa7fbf998b9706d75
noha <nohaden12@gmail.com>
2026-04-26 19:57:27 +0300

    added get and update movie by id

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-    // GET /movies
-    if (req.method === 'GET' && req.url === '/movies') {
+     if (req.method === 'GET' && req.url === '/movies') {
         const data = fs.readFileSync('file.json', 'utf8');
         res.setHeader('Content-Type', 'application/json');
-        res.end(data);
+        return res.end(data);
     }
 
-    // POST /movies
-    else if (req.method === 'POST' && req.url === '/movies') {
+     else if (req.method === 'GET' && req.url.startsWith('/movies/')) {
+        const id = req.url.split('/')[2];
+
+        let data = JSON.parse(fs.readFileSync('file.json', 'utf8'));
+
+        const movie = data.find(m => m.id == id);
+
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(movie));
+    }
+
+     else if (req.method === 'POST' && req.url === '/movies') {
         let body = '';
 
         req.on('data', chunk => {
@@ -21,8 +30,7 @@ const server = http.createServer((req, res) => {
         req.on('end', () => {
             const newMovie = JSON.parse(body);
 
-            let data = fs.readFileSync('file.json', 'utf8');
-            data = JSON.parse(data);
+            let data = JSON.parse(fs.readFileSync('file.json', 'utf8'));
 
             newMovie.id = Date.now();
             data.push(newMovie);
@@ -34,7 +42,33 @@ const server = http.createServer((req, res) => {
         });
     }
 
-    // fallback route
+     else if (req.method === 'PUT' && req.url.startsWith('/movies/')) {
+        const id = req.url.split('/')[2];
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            const updatedData = JSON.parse(body);
+
+            let data = JSON.parse(fs.readFileSync('file.json', 'utf8'));
+
+            data = data.map(m => {
+                if (m.id == id) {
+                    return { ...m, ...updatedData };
+                }
+                return m;
+            });
+
+            fs.writeFileSync('file.json', JSON.stringify(data, null, 2));
+
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: "Updated" }));
+        });
+    }
+
     else {
         res.statusCode = 404;
         res.end("Route not found");
